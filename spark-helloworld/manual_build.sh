295374f8281c7f95c07e716051e65a1453cdd098
Solomon X. <solomonxiewise@gmail.com>
2026-08-19 10:14:59 -0700

    fix install

--- spark-helloworld/manual_build.sh
+++ spark-helloworld/manual_build.sh
@@ -1,23 +1,25 @@
-# Prepare EC2: t2.small, 1 vCPU, 2GB memory, 64bit x86, Ubuntu 26.04
+# Prepare EC2: t3.small, 1 vCPU, 2GB memory, 64bit x86, Ubuntu 26.04
 
 
 ssh ubuntu@ec2-xxxxxxxx.compute-1.amazonaws.com -i ~/.ssh/mykeypairxxxxx.pem
 
-sudo apt update && sudo apt upgrade -y
-sudo apt install -y openjdk-11-jdk-headless wget tar
+sudo apt update
+sudo apt install -y openjdk-17-jdk-headless wget tar
 sudo apt install -y python3-pip python3-venv
 
-pip3 install --quiet --break-system-packages pyspark pandas
+pip3 install --quiet --break-system-packages --use-pep517 pyspark
+pip3 install --quiet pandas
 
-wget -q https://archive.apache.org/dist/spark/spark-3.5.0/spark-3.5.0-bin-hadoop3.tgz
-tar -xzf spark-3.5.0-bin-hadoop3.tgz
-sudo mv spark-3.5.0-bin-hadoop3 /opt/spark
-rm spark-3.5.0-bin-hadoop3.tgz
-chown -R ubuntu:ubuntu /opt/spark
+wget https://archive.apache.org/dist/spark/spark-4.2.0/spark-4.2.0-bin-hadoop3.tgz
+tar -xzf spark-4.2.0-bin-hadoop3.tgz
+sudo mv spark-4.2.0-bin-hadoop3 /opt/spark
+rm spark-4.2.0-bin-hadoop3.tgz
+ls -lah /opt/spark
 
+export PYSPARK_PYTHON=python3
 export SPARK_HOME=/opt/spark
 export PATH=$PATH:$SPARK_HOME/bin:$SPARK_HOME/sbin
-export PYSPARK_PYTHON=python3
+echo $PATH
 
 
 /opt/spark/sbin/start-master.sh
